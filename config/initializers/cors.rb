6891ef0b31003da7ab4ac6ad5093465052ff3802
Ajinkya Shejul <ajinkyashejul4195@gmail.com>
2024-02-14 07:56:32 +0530

    CORS Update

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,10 +7,12 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins ['http://localhost:3001','https://gumroad-clone.onrender.com']
+    origins /http:\/\/localhost:\d+$/, /https:\/\/.*\.onrender\.com$/
 
     resource "*",
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head]
   end
 end
+
+
