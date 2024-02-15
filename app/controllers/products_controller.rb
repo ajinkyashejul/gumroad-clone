bf3132fcc5e84e8c8c56dc2ace94e4ac86ffff0a
Ajinkya Shejul <ajinkyashejul@Ajinkyas-MacBook-Air.local>
2024-02-15 21:48:04 +0530

    Get only relevant columns from MOngoDB to speedup API call

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,13 +1,19 @@
-# app/controllers/products_controller.rb
 class ProductsController < ApplicationController
-    def index
-      if params[:creator].present?
-        @products = Product.where('creator' => params[:creator]).select(:name, :ratings, :price, :thumbnail_url, :is_template_bundle)
-      else
-        @products = Product.all.select(:name, :ratings, :price, :thumbnail_url, :is_template_bundle)
-      end
-  
-      render json: @products
+  def index
+    if params[:creator].present?
+      puts :creator
+      product_data = Product.where(creator: params[:creator])
+    else
+      product_data = Product.all
     end
+
+    columns_to_pluck = [:name, :price, :ratings, :thumbnail_url, :is_template_bundle]
+    plucked_data = product_data.pluck(*columns_to_pluck)
+    # Convert plucked data into an array of hashes
+    @products = plucked_data.map do |data|
+      columns_to_pluck.zip(data).to_h
+    end
+
+    render json: @products
   end
-  
+end
