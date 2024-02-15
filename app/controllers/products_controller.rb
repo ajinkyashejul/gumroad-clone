ebca2f9e0ce9a6ac73d68afd70dc31ef9c67b670
Ajinkya Shejul <ajinkyashejul@Ajinkyas-MacBook-Air.local>
2024-02-15 21:18:16 +0530

    Use banckend API

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,9 +2,9 @@
 class ProductsController < ApplicationController
     def index
       if params[:creator].present?
-        @products = Product.where('creator' => params[:creator])
+        @products = Product.where('creator' => params[:creator]).select(:name, :ratings, :price, :thumbnail_url, :is_template_bundle)
       else
-        @products = Product.all
+        @products = Product.all.select(:name, :ratings, :price, :thumbnail_url, :is_template_bundle)
       end
   
       render json: @products
